7e0a53567e56ed83b60e40a4b959b94efc35b6df
Your Name <570285411@qq.com>
2021-03-16 17:04:09 +0800

    个人中心新动画添加，页面路径配置

--- Muzic/miniprogram/pages/index/index.js
+++ Muzic/miniprogram/pages/index/index.js
@@ -4,6 +4,8 @@ Page({
   data:{
     bannerList:[],
     recommendList:[],
+    topList:[],
+    topListSong:[]
   },
   
   onLoad: async function () {
@@ -15,5 +17,29 @@ Page({
     this.setData({
       recommendList: commendListData.result
     })
+// 排行榜，获取排行榜详情获取
+      let reslutArr = [];//所欲榜单内容
+      let topListData = await request('/toplist');
+      let allLists = topListData.list; //所有榜单
+      for(let i = 0,len  = allLists.length;i < len; i++){
+        let name = allLists[i].name;
+        let id = allLists[i].id
+        let topListItem  = {name:name,id:id}
+        reslutArr.push(topListItem);  
+        this.setData({
+          topList:reslutArr
+        })
+      }
+      let id = reslutArr[0].id
+      let index = 0
+      for(let q = 0;q < 30;q++){
+        let playlistData = await request('/playlist/detail',{id:id})
+        // console.log(playlistData)
+        this.setData({
+          topListSong:playlistData.playlist.tracks.slice(0,5),
+        })
+      }
+      
+// 排行榜
   },
 })
---
+++ Muzic/miniprogram/pages/personal/index.js
@@ -0,0 +1,81 @@
+let startY = 0;
+let moveY = 0;
+let moveDistance = 0;
+Page({
+    data: {
+        coverTransform:'translateY(0rpx)',
+        coveTransition:''
+    },
+    onLoad: function (options) {
+    },
+    handleTouchStart(event){
+        this.setData({
+            coveTransition:''
+        })
+        startY = event.touches[0].clientY; // 获取手指的起始坐标
+    },
+    handleTouchMove(event){
+        moveY = event.touches[0].clientY
+        moveDistance = moveY - startY
+        if(moveDistance<0){
+            return 
+        }else if(moveDistance >= 80){
+            moveDistance = 100
+        }
+        this.setData({
+            coverTransform:`translateY(${moveDistance}rpx)`
+            
+        })
+    },
+    handleTouchEnd(){
+        this.setData({
+            coverTransform:`translateY(0rpx))`,
+            coveTransition:`transform 0.5s linear`
+        })
+    },
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
